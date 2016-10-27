9834b96eda1c6336d05ad7513f368ca91f5a324d
ErikUu <erik.uusitalo@gmail.com>
2016-10-27 11:32:35 +0200

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by erikuusitalo on 27/10/16.
+ */
+public class Main {
+
+    public static void main(String argv[]) {
+
+        System.out.println("hello gradle");
+
+    }
+
+
+}
---
+++ src/main/java/Player.java
@@ -0,0 +1,5 @@
+/**
+ * Created by erikuusitalo on 27/10/16.
+ */
+public class Player {
+}
