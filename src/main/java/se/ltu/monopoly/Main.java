7026e6eb20c2952b3a376557346f88f38dd6967e
ErikUu <erik.uusitalo@gmail.com>
2016-10-28 16:23:40 +0200

    Update main

--- src/main/java/se/ltu/monopoly/Main.java
+++ src/main/java/se/ltu/monopoly/Main.java
@@ -1,6 +1,11 @@
 package se.ltu.monopoly;
 
-import java.util.Vector;
+import se.ltu.monopoly.Tiles.*;
+import se.ltu.monopoly.Tiles.ownable.Classroom;
+import se.ltu.monopoly.Tiles.ownable.Philm;
+import se.ltu.monopoly.Tiles.ownable.Stil;
+
+import java.util.ArrayList;
 
 /**
  * Created by haidar on 2016-10-27.
@@ -14,42 +19,41 @@ public class Main {
 
     public static void main(String[] argv) {
 
-
+        // check system arguments
         if(argv.length < 1 ||argv.length > 4) {
-
             System.out.println("Syntax: Monopoly [Player 1-4 initials]\nexample: Monopoly F.L J.H K.S");
             System.exit(0);
-
-        } else {
-
-            Board b = new Board();
-            b.setmPlayers(parsePlayers(argv));
-            b.setmDice(new Dice(6));
-
         }
+
+
+        // board settings
+        BoardBuilder bb = new BoardBuilder();
+        bb.setPlayers(argv);
+        bb.setChanceCards(new String[]{
+                "Library"
+        });
+        bb.setTiles(new ArrayList<Tile>() {{
+            add(new Start());
+            add(new Stil(2,6));
+            add(new Chance());
+            add(new Philm(2,6));
+            add(new Party(8, 18));
+            add(new Classroom("A109", 3, 3, 10));
+            add(new Classroom("A117", 3, 3, 10));
+            add(new Library());
+            add(new Classroom("B234Ske", 3, 3, 10));
+            add(new Chance());
+            add(new Classroom("E632", 3, 3, 10));
+            add(new Exam());
+            add(new Classroom("A209", 5, 4, 20));
+            add(new Classroom("A210", 5, 4, 20));
+        }});
+        bb.setDice(6);
+
+        // start board
+        bb.createBoard().start();
+
 
     }
 
-    /**
-     *
-     * @param playerNames
-     * @return vector of players
-     */
-    private static Vector<Player> parsePlayers(String[] playerNames) {
-
-        Vector<Player> players = new Vector<Player>();
-
-        int computerID = 1;
-        for(int i=0; i<4; i++) {
-            if(playerNames.length >= (i+1)) {
-                players.add(new Player(playerNames[i]));
-            } else {
-                //C.1 = computer 1, etc.
-                players.add(new Player("C." + computerID, true));
-                computerID++;
-            }
-        }
-
-        return players;
-    }
 }
