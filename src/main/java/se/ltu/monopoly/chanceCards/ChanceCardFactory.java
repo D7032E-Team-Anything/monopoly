8189ad6e95b3b8d2c587570445a34d084b9db39a
ErikUu <erik.uusitalo@gmail.com>
2016-10-28 16:22:24 +0200

    Fix chance card factory

--- src/main/java/se/ltu/monopoly/chanceCards/ChanceCardFactory.java
+++ src/main/java/se/ltu/monopoly/chanceCards/ChanceCardFactory.java
@@ -5,26 +5,12 @@ package se.ltu.monopoly.chanceCards;
  */
 public class ChanceCardFactory {
 
-    ChanceCard chanceCard = null;
-
     public ChanceCard getChanceCard(String className) throws ClassNotFoundException{
         if(className.equals("Library")) {
             return new Library();
         }
-        throw new ClassNotFoundException(className + " not fot a class");
+        throw new ClassNotFoundException(className + " is not a class");
     }
 
 
-    private ChanceCard initChanceCard(String className) {
-        try {
-            chanceCard = (ChanceCard)Class.forName(className).newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return chanceCard;
-    }
 }
